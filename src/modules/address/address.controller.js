a08b92cd0982b6bfe1502f674fa35bd7ab971348
Menna23Shehata <menna.sh.ahmed2023@gmail.com>
2023-10-30 18:17:34 +0200

    new update

--- src/modules/address/address.controller.js
+++ src/modules/address/address.controller.js
@@ -2,16 +2,14 @@ import { AppError } from "../../utils/services/AppError.js"
 import catchAsyncError from "../../utils/midlleware/catchAsyncError.js"
 import { userModel } from "../../../db/models/user.model.js"
 
-const addAddress = 
-catchAsyncError(
+const addAddress = catchAsyncError(
     async (req, res, next) => {
 
     let results = await userModel.findByIdAndUpdate(req.user._id, { $addToSet: { address: req.body } },
         { new: true });
     !results && next(new AppError("can't find product", 404));
     results && res.json({ message: "Done", results: results.address });
-}
-);
+});
 
 
 const deleteAddress = catchAsyncError(async (req, res, next) => {
